3f64cb257ae018ec09aaaab3dc140161f503efee
roynulrohan <roynulrohan@gmail.com>
2025-02-13 08:06:31 +0000

    Remove TanStack Router Devtools from root route

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,11 +1,9 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router';
-import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 
 function Root() {
     return (
         <>
             <Outlet />
-            <TanStackRouterDevtools />
         </>
     );
 }
